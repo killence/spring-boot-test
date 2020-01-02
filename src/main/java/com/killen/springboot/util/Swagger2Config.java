81b27d3d209c02e62df29cec9b373dea9ac11726
killence <1654401766@qq.com>
2020-01-02 09:59:21 +0800

    Spring Boot之学习Swagger2，API文档

---
+++ src/main/java/com/killen/springboot/util/Swagger2Config.java
@@ -0,0 +1,45 @@
+package com.killen.springboot.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @ClassName Swagger2Config
+ * @Description: TODO
+ * @Author killen
+ * @Date 2020-01-02
+ * @Version V1.0
+ **/
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.killen.springboot"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("客户管理")
+                .description("客户管理中⼼ API 1.0 操作⽂档")
+                //服务条款⽹址
+                .termsOfServiceUrl("http://www.ityouknow.com/")
+                .version("1.0")
+                .contact(new Contact("纯洁的微笑", "http://www.ityouknow.com/", "ityouknow@126.com"))
+                .build();
+    }
+}
